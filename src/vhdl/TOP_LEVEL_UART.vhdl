4d1fde225fa6c16100213dbd87f30b155caae5c6
Rafael Romón <rafaelromonsagredo@protonmail.com>
2021-04-19 11:03:02 +0200

    Implement new UART entity
    Implemented new UART entity sent by Sven, still not working

--- src/vhdl/TOP_LEVEL_UART.vhdl
+++ src/vhdl/TOP_LEVEL_UART.vhdl
@@ -1,6 +1,6 @@
 -----------------------------------------------------
 -- Title: TOP_LEVEL_UART.vhdl
--- Author: Rafael Romón/NN-1
+-- Author: Rafael Romon/NN-1
 -- DAT096 - spring 2021
 -----------------------------------------------------
 -- Description:
@@ -21,7 +21,7 @@ USE UNISIM.Vcomponents.ALL;
 ENTITY TOP_LEVEL_UART IS
 	GENERIC
 	(
-		clk_freq  : INTEGER   := 100 --system clock frequency in MHz
+		clk_freq : INTEGER := 100 --system clock frequency in MHz
 	);
 	PORT
 	(
@@ -40,26 +40,30 @@ END TOP_LEVEL_UART;
 ARCHITECTURE TOP_LEVEL_UART_arch OF TOP_LEVEL_UART IS
 
 	TYPE states IS (Idle, SendResult, Finished);
-	SIGNAL state_machine : states := Idle;
-	SIGNAL clk           : STD_LOGIC;
-	SIGNAL reset_signal  : STD_LOGIC                    := '0';
+	SIGNAL state_machine  : states := Idle;
+	SIGNAL clk            : STD_LOGIC;
+	SIGNAL reset_p        : STD_LOGIC                    := '0';
+	SIGNAL reset_n        : STD_LOGIC                    := '1';
 
-	SIGNAL TX_DV_signal         : STD_LOGIC                    := '0';
-	SIGNAL TX_Byte_signal       : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-	SIGNAL TX_Done_signal       : STD_LOGIC;
+	SIGNAL tx_ena_signal  : STD_LOGIC                    := '0';
+	SIGNAL tx_busy_signal : STD_LOGIC                    := '0';
+	SIGNAL tx_data_signal : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
 
-	COMPONENT UART_TX IS
+	COMPONENT uart
 		PORT
 		(
-			clk       : IN  STD_LOGIC;
-			TX_DV     : IN  STD_LOGIC;
-			TX_Byte   : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
-			TX_Active : OUT STD_LOGIC;
-			TX_Serial : OUT STD_LOGIC;
-			TX_Done   : OUT STD_LOGIC
+			clk      : IN  STD_LOGIC;                    --system clock
+			reset_n  : IN  STD_LOGIC;                    --ascynchronous reset
+			tx_ena   : IN  STD_LOGIC;                    --initiate transmission
+			tx_data  : IN  STD_LOGIC_VECTOR(7 DOWNTO 0); --data to transmit
+			rx       : IN  STD_LOGIC;                    --receive pin
+			rx_busy  : OUT STD_LOGIC;                    --data reception in progress
+			rx_error : OUT STD_LOGIC;                    --start, parity, or stop bit error detected
+			rx_data  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0); --data received
+			tx_busy  : OUT STD_LOGIC;                    --transmission in progress
+			tx       : OUT STD_LOGIC                   --transmit pin
 		);
-	END COMPONENT UART_TX;
-
+	END COMPONENT uart;
 BEGIN
 
 	-- LVDS input to internal single
@@ -68,33 +72,36 @@ BEGIN
 	MAP(
 	IOSTANDARD => "DEFAULT"
 	)
-	PORT
-	MAP
+	PORT MAP
 	(
-	I  => SYSCLK_P,
-	IB => SYSCLK_N,
-	O  => clk
+		I  => SYSCLK_P,
+		IB => SYSCLK_N,
+		O  => clk
 	);
 
-	UART_TX_comp : UART_TX -- Instantiate UART transmitter
+	uart_i : uart
 	PORT
-	MAP
-	(
-	clk       => clk,
-	TX_DV     => TX_DV_signal,
-	TX_Byte   => TX_Byte_signal,
-	TX_Active => OPEN,
-	TX_Serial => USB_UART_TX,
-	TX_Done   => TX_Done_signal
+	MAP (
+	clk      => clk,
+	reset_n  => reset_n,
+	tx_ena   => tx_ena_signal,
+	tx_data  => tx_data_signal,
+	rx       => '1', -- Always high so no receive
+	rx_busy  => OPEN,
+	rx_error => OPEN,
+	rx_data  => OPEN,
+	tx_busy  => tx_busy_signal,
+	tx       => USB_UART_TX
 	);
-
 	-- Purpose: Control state machine
 	TOP_LEVEL_UART_process : PROCESS (clk, CPU_RESET)
 		VARIABLE start_flag : STD_LOGIC := '0';
 		VARIABLE sent_flag  : STD_LOGIC := '0';
 	BEGIN
 		IF CPU_RESET = '1' THEN
-			reset_signal  <= '1';
+			reset_p       <= '1';
+			reset_n       <= '0';
+
 			state_machine <= Idle;
 
 			GPIO_LED_0    <= '1';
@@ -103,12 +110,16 @@ BEGIN
 			GPIO_LED_3    <= '1';
 
 		ELSIF RISING_EDGE(clk) THEN
+
+			reset_p <= '0';
+			reset_n <= '1';
+
 			CASE state_machine IS
 				WHEN Idle =>
 
 					GPIO_LED_0 <= '0';
-					GPIO_LED_1 <= '0';
-					GPIO_LED_2 <= '1';
+					GPIO_LED_1 <= '1';
+					GPIO_LED_2 <= '0';
 					GPIO_LED_3 <= '0';
 
 					IF GPIO_SW_N = '1' THEN
@@ -124,16 +135,20 @@ BEGIN
 					GPIO_LED_2 <= '1';
 					GPIO_LED_3 <= '0';
 
-					IF sent_flag = '0' THEN						
-						TX_DV_signal   <= '1';
-						TX_Byte_signal <= "01010101";
+					IF sent_flag = '0' AND tx_busy_signal = '0' THEN
+						tx_ena_signal  <= '1';
+						tx_data_signal <= "01010101";
 						sent_flag := '1';
-					ELSIF sent_flag = '1' and TX_Done_signal = '0' THEN
-						TX_DV_signal <= '0';
-					ELSE
-					    sent_flag := '0';
-						state_machine <= Finished;
+					ELSIF sent_flag = '1' THEN
+					   tx_ena_signal <= '0';
+					   IF tx_busy_signal = '1' THEN
+							tx_ena_signal <= '0';
+					   ELSE
+							sent_flag := '0';
+							state_machine <= Finished;
+						END IF;
 					END IF;
+
 				WHEN Finished =>
 					GPIO_LED_0 <= '0';
 					GPIO_LED_1 <= '0';
