ca9960828ee4a968c85449ada318b1ed2461884e
Rafael Romón <rafaelromonsagredo@protonmail.com>
2021-05-06 10:55:47 +0200

    Finish unifying top level
    wired and tested all componentes in top level, reduced size of image buffer to reduce resource utilization

--- src/vhdl/IMG_BUFFER_CONT.vhdl
+++ src/vhdl/IMG_BUFFER_CONT.vhdl
@@ -18,15 +18,16 @@ USE ieee.numeric_std.ALL;
 
 ENTITY IMG_BUFFER_CONTROLLER IS
 	GENERIC (
-		ROW_WIDTH : INTEGER := 4096;
-		IMAGE_DEPTH : INTEGER := 4;
-		ADDR_WIDTH : INTEGER := 9
+		ROW_WIDTH : INTEGER := 128;
+		IMAGE_DEPTH : INTEGER := 128;
+		ADDR_WIDTH : INTEGER := 11
 	);
 	PORT (
 		clk : IN STD_LOGIC; -- clock signal
 		reset_p : IN STD_LOGIC; -- reset signal active high
 		start : IN STD_LOGIC; -- signal to start reading new image
 		busy : OUT STD_LOGIC; -- signal indicating controller is busy reading new image
+		done : OUT STD_LOGIC;
 		image : OUT STD_LOGIC_VECTOR((ROW_WIDTH * IMAGE_DEPTH) - 1 DOWNTO 0) -- output image
 	);
 END IMG_BUFFER_CONTROLLER;
@@ -36,26 +37,26 @@ ARCHITECTURE IMG_BUFFER_CONTROLLER_arch OF IMG_BUFFER_CONTROLLER IS
 	SIGNAL base_addr : INTEGER := 0;
 	SIGNAL temp_img : STD_LOGIC_VECTOR((ROW_WIDTH * IMAGE_DEPTH) - 1 DOWNTO 0);
 
-	SIGNAL addra : STD_LOGIC_VECTOR (5 DOWNTO 0) := "000000";
-	SIGNAL douta : STD_LOGIC_VECTOR (4095 DOWNTO 0);
+	SIGNAL addra : STD_LOGIC_VECTOR (ADDR_WIDTH-1 DOWNTO 0) := "00000000000";
+	SIGNAL douta : STD_LOGIC_VECTOR (ROW_WIDTH-1 DOWNTO 0);
 
-	SIGNAL addrb : STD_LOGIC_VECTOR (5 DOWNTO 0) := "000001";
-	SIGNAL doutb : STD_LOGIC_VECTOR (4095 DOWNTO 0);
+	SIGNAL addrb : STD_LOGIC_VECTOR (ADDR_WIDTH-1 DOWNTO 0) := "00000000001";
+	SIGNAL doutb : STD_LOGIC_VECTOR (ROW_WIDTH-1 DOWNTO 0);
 
 	COMPONENT image_buffer
 		PORT (
 			clka : IN STD_LOGIC;
 			ena : IN STD_LOGIC;
 			wea : IN STD_LOGIC_VECTOR (0 TO 0);
-			addra : IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-			dina : IN STD_LOGIC_VECTOR (4095 DOWNTO 0);
-			douta : OUT STD_LOGIC_VECTOR (4095 DOWNTO 0);
+			addra : IN STD_LOGIC_VECTOR (ADDR_WIDTH-1 DOWNTO 0);
+			dina : IN STD_LOGIC_VECTOR (ROW_WIDTH-1 DOWNTO 0);
+			douta : OUT STD_LOGIC_VECTOR (ROW_WIDTH-1 DOWNTO 0);
 			clkb : IN STD_LOGIC;
 			enb : IN STD_LOGIC;
 			web : IN STD_LOGIC_VECTOR (0 TO 0);
-			addrb : IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-			dinb : IN STD_LOGIC_VECTOR (4095 DOWNTO 0);
-			doutb : OUT STD_LOGIC_VECTOR (4095 DOWNTO 0)
+			addrb : IN STD_LOGIC_VECTOR (ADDR_WIDTH-1 DOWNTO 0);
+			dinb : IN STD_LOGIC_VECTOR (ROW_WIDTH-1 DOWNTO 0);
+			doutb : OUT STD_LOGIC_VECTOR (ROW_WIDTH-1 DOWNTO 0)
 		);
 	END COMPONENT image_buffer;
 BEGIN
@@ -75,24 +76,29 @@ BEGIN
 		dinb => (OTHERS => '0'),
 		doutb => doutb
 	);
+	
 	IMG_BUFFER_CONTROLLER_process : PROCESS (clk, reset_p)
 		VARIABLE start_flag : STD_LOGIC := '0';
 		VARIABLE read_flag : STD_LOGIC := '0';
 		VARIABLE word_count : INTEGER := 0;
-		VARIABLE up_limit : INTEGER;
-		VARIABLE low_limit : INTEGER;
+		VARIABLE vector_pos : INTEGER;
 	BEGIN
 
-		IF reset_p = '1' THEN
+		IF reset_p = '1' THEN		   
 			base_addr <= 0;
 			temp_img <= (OTHERS => '0');
+			
+			busy <= '0';
+			done <= '0';			
 			image <= (OTHERS => '0');
 
 		ELSIF RISING_EDGE(clk) THEN
 			IF start = '1' THEN
-				busy <= '1';
-				start_flag := '1';
-				up_limit := (ROW_WIDTH * IMAGE_DEPTH) - 1;
+			    start_flag := '1';
+				vector_pos := (ROW_WIDTH * IMAGE_DEPTH) - 1;
+				word_count := 0;
+			    done <= '0';
+				busy <= '1';			
 			END IF;
 
 			IF start_flag = '1' THEN
@@ -100,19 +106,21 @@ BEGIN
 				IF word_count < IMAGE_DEPTH THEN
 
 					addra <= STD_LOGIC_VECTOR(to_unsigned(base_addr + word_count, addra'length));
-					addrb <= STD_LOGIC_VECTOR(to_unsigned(base_addr + word_count + 1, addrb'length));
+					addrb <= STD_LOGIC_VECTOR(to_unsigned(base_addr + word_count + 1, addrb'length));					
 
-					low_limit := up_limit - (ROW_WIDTH * 2);
-
-					temp_img(up_limit DOWNTO low_limit + 1) <= douta & doutb;
-
+					temp_img(vector_pos DOWNTO vector_pos-ROW_WIDTH+1) <= douta;					
+					vector_pos := vector_pos - ROW_WIDTH - 1;
+					
+				    temp_img(vector_pos DOWNTO vector_pos-ROW_WIDTH+1) <= doutb;
+                    vector_pos := vector_pos - ROW_WIDTH - 1;
+                    
 					word_count := word_count + 2;
-					up_limit := low_limit;
 
 				ELSE
-					image <= temp_img;
-					busy <= '0';
 					start_flag := '0';
+					busy <= '0';
+					done <= '1';
+					image <= temp_img;										
 				END IF;
 			END IF;
 		END IF;
--- src/vhdl/TOP_LEVEL.vhdl
+++ src/vhdl/TOP_LEVEL.vhdl
@@ -21,20 +21,17 @@ LIBRARY UNISIM;
 USE UNISIM.Vcomponents.ALL;
 
 ENTITY TOP_LEVEL IS
-	GENERIC (
-		clk_freq : INTEGER := 100; --system clock frequency in MHz
-		testbench : STD_LOGIC := '0' --disables debouncer and waits for testbench
-	);
-	PORT (
-		SYSCLK_P : IN STD_LOGIC;
-		SYSCLK_N : IN STD_LOGIC;
-		CPU_RESET : IN STD_LOGIC;
-		GPIO_SW_N : IN STD_LOGIC;
+	PORT
+	(
+		SYSCLK_P    : IN  STD_LOGIC;
+		SYSCLK_N    : IN  STD_LOGIC;
+		CPU_RESET   : IN  STD_LOGIC;
+		GPIO_SW_N   : IN  STD_LOGIC;
 		USB_UART_TX : OUT STD_LOGIC;
-		GPIO_LED_0 : OUT STD_LOGIC;
-		GPIO_LED_1 : OUT STD_LOGIC;
-		GPIO_LED_2 : OUT STD_LOGIC;
-		GPIO_LED_3 : OUT STD_LOGIC
+		GPIO_LED_0  : OUT STD_LOGIC;
+		GPIO_LED_1  : OUT STD_LOGIC;
+		GPIO_LED_2  : OUT STD_LOGIC;
+		GPIO_LED_3  : OUT STD_LOGIC
 	);
 END TOP_LEVEL;
 
@@ -42,131 +39,118 @@ ARCHITECTURE TOP_LEVEL_arch OF TOP_LEVEL IS
 
 	TYPE states IS (Idle, LoadImage, ProcessImage, SendResult);
 	SIGNAL state_machine : states := Idle;
-	SIGNAL clk : STD_LOGIC;
-	SIGNAL reset_signal : STD_LOGIC := '0';
+	SIGNAL clk           : STD_LOGIC;
+	SIGNAL reset_signal  : STD_LOGIC := '0';
 
-	SIGNAL reset_p : STD_LOGIC := '0';
-	SIGNAL start_signal : STD_LOGIC := '0';
+	SIGNAL start_signal  : STD_LOGIC := '0';
+	
+	SIGNAL img_cont_start : STD_LOGIC := '1';
+	SIGNAL img_cont_busy  : STD_LOGIC;
+	SIGNAL img_cont_done  : STD_LOGIC;
 
-	COMPONENT button_debounce IS
-		GENERIC (
-			clk_freq : INTEGER := 10; --system clock frequency in MHz
-			stable_time : INTEGER := 10); --time button must remain stable in ms
-		PORT (
-			clk : IN STD_LOGIC;
-			reset_p : IN STD_LOGIC;
-			button : IN STD_LOGIC;
-			result : OUT STD_LOGIC
-		);
-	END COMPONENT button_debounce;
+	COMPONENT IMG_BUFFER_CONTROLLER
+	PORT (
+	  clk     : IN  STD_LOGIC;
+	  reset_p : IN  STD_LOGIC;
+	  start   : IN  STD_LOGIC;
+	  busy    : OUT STD_LOGIC;
+	  done    : OUT STD_LOGIC;
+	  image   : OUT STD_LOGIC_VECTOR(16383 DOWNTO 0)
+	);
+	END COMPONENT IMG_BUFFER_CONTROLLER;
 
-	SIGNAL cnn_start : STD_LOGIC := '0';
-	SIGNAL loaded_image : STD_LOGIC_VECTOR(16383 DOWNTO 0);
-	SIGNAL cnn_finished : STD_LOGIC;
-	SIGNAL cnn_result : STD_LOGIC_VECTOR(5 DOWNTO 0);
+	SIGNAL cnn_start     : STD_LOGIC := '0';
+	SIGNAL loaded_image  : STD_LOGIC_VECTOR(16383 DOWNTO 0);
+	SIGNAL cnn_finished  : STD_LOGIC;
+	SIGNAL cnn_result    : STD_LOGIC_VECTOR(5 DOWNTO 0);
 
 	COMPONENT CNN IS
-		PORT (
-			clk : IN STD_LOGIC;
-			reset_p : IN STD_LOGIC;
-			start : IN STD_LOGIC;
-			image : IN STD_LOGIC_VECTOR(16383 DOWNTO 0);
+		PORT
+		(
+			clk      : IN  STD_LOGIC;
+			reset_p  : IN  STD_LOGIC;
+			start    : IN  STD_LOGIC;
+			image    : IN  STD_LOGIC_VECTOR(16383 DOWNTO 0);
 			finished : OUT STD_LOGIC;
-			result : OUT STD_LOGIC_VECTOR(5 DOWNTO 0)
+			result   : OUT STD_LOGIC_VECTOR(5 DOWNTO 0)
 		);
 	END COMPONENT CNN;
 
-	SIGNAL TX_DV : STD_LOGIC := '0';
-	SIGNAL TX_Byte : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-	SIGNAL TX_Done : STD_LOGIC;
+	SIGNAL uart_start   : STD_LOGIC                    := '0';
+	SIGNAL uart_msg : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+	SIGNAL uart_busy : STD_LOGIC;
+	SIGNAL uart_done : STD_LOGIC;
 
 	COMPONENT UART_TX IS
-		PORT (
-			clk : IN STD_LOGIC;
-			TX_DV : IN STD_LOGIC;
-			TX_Byte : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		PORT
+		(
+			clk       : IN  STD_LOGIC;
+			TX_DV     : IN  STD_LOGIC;
+			TX_Byte   : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
 			TX_Active : OUT STD_LOGIC;
 			TX_Serial : OUT STD_LOGIC;
-			TX_Done : OUT STD_LOGIC
+			TX_Done   : OUT STD_LOGIC
 		);
 	END COMPONENT UART_TX;
-
 BEGIN
 
 	-- LVDS input to internal single
 	CLK_IBUFDS : IBUFDS
-	GENERIC MAP(
-		IOSTANDARD => "DEFAULT"
+	GENERIC
+	MAP(
+	IOSTANDARD => "DEFAULT"
 	)
 	PORT MAP
 	(
-		I => SYSCLK_P,
+		I  => SYSCLK_P,
 		IB => SYSCLK_N,
-		O => clk
+		O  => clk
 	);
+
+	IMG_BUFFER_CONTROLLER_i : IMG_BUFFER_CONTROLLER
+	PORT MAP
+	(
+  	clk     => clk,
+  	reset_p => reset_signal,
+  	start   => img_cont_start,
+  	busy    => img_cont_busy,
+  	done    => img_cont_done,
+  	image   => loaded_image
+	);
+
 
 	CNN_comp : CNN -- Instantiate CNN transmitter
 	PORT MAP
 	(
-		clk => clk,
-		reset_p => reset_signal,
-		start => cnn_start,
-		image => loaded_image,
-		finished => cnn_finished,
-		result => cnn_result
+        clk      => clk,
+        reset_p  => reset_signal,
+        start    => cnn_start,
+        image    => loaded_image,
+        finished => cnn_finished,
+        result   => cnn_result
 	);
 
 	UART_TX_comp : UART_TX -- Instantiate UART transmitter
 	PORT MAP
 	(
-		clk => clk,
-		TX_DV => TX_DV,
-		TX_Byte => TX_Byte,
-		TX_Active => OPEN,
-		TX_Serial => USB_UART_TX,
-		TX_Done => TX_Done
-	);
-
-	reset_debounce_comp : button_debounce -- debouncer for CPU_RESET
-	PORT MAP
-	(
-		clk => clk,
-		reset_p => '0',
-		button => CPU_RESET,
-		result => reset_p
-	);
-	start_debounce_comp : button_debounce -- debouncer for GPIO_SW_N
-	PORT MAP
-	(
-		clk => clk,
-		reset_p => reset_p,
-		button => GPIO_SW_N,
-		result => start_signal
+        clk       => clk,
+        TX_DV     => uart_start,
+        TX_Byte   => uart_msg,
+        TX_Active => uart_busy,
+        TX_Serial => USB_UART_TX,
+        TX_Done   => uart_done
 	);
 
 	LED_indicator_process : PROCESS (clk)
-		VARIABLE count : INTEGER RANGE 0 TO clk_freq * 100000; -- 100ms
-		VARIABLE blinking_led : STD_LOGIC := '1';
 	BEGIN
-		IF reset_p = '1' THEN
+		IF CPU_RESET = '1' THEN
 			GPIO_LED_0 <= '1';
 			GPIO_LED_1 <= '1';
 			GPIO_LED_2 <= '1';
 			GPIO_LED_3 <= '1';
 
 		ELSIF RISING_EDGE(clk) THEN
-
-			IF testbench = '0' THEN
-				IF (count < clk_freq * 100000) THEN
-					count := count + 1;
-				ELSE
-					blinking_led := NOT(blinking_led);
-					count := 0;
-				END IF;
-			ELSE
-				blinking_led := '1';
-			END IF;
-
+					
 			CASE state_machine IS
 				WHEN Idle =>
 					GPIO_LED_0 <= '1';
@@ -176,7 +160,7 @@ BEGIN
 
 				WHEN LoadImage =>
 					GPIO_LED_0 <= '0';
-					GPIO_LED_1 <= blinking_led;
+					GPIO_LED_1 <= '1';
 					GPIO_LED_2 <= '0';
 					GPIO_LED_3 <= '0';
 
@@ -184,7 +168,7 @@ BEGIN
 
 					GPIO_LED_0 <= '0';
 					GPIO_LED_1 <= '0';
-					GPIO_LED_2 <= blinking_led;
+					GPIO_LED_2 <= '1';
 					GPIO_LED_3 <= '0';
 
 				WHEN SendResult =>
@@ -192,7 +176,7 @@ BEGIN
 					GPIO_LED_0 <= '0';
 					GPIO_LED_1 <= '0';
 					GPIO_LED_2 <= '0';
-					GPIO_LED_3 <= blinking_led;
+					GPIO_LED_3 <= '1';
 
 			END CASE;
 		END IF;
@@ -200,70 +184,56 @@ BEGIN
 
 	-- Purpose: Control state machine
 	TOP_LEVEL_process : PROCESS (clk, CPU_RESET)
-		VARIABLE count : INTEGER RANGE 0 TO clk_freq * 5000000; -- 5s
-		VARIABLE start_flag : STD_LOGIC;
+		VARIABLE start_flag : STD_LOGIC := '0';
+		VARIABLE sent_flag  : STD_LOGIC := '0';
 	BEGIN
-		IF reset_p = '1' OR (testbench = '1' AND CPU_RESET = '1') THEN
-			reset_signal <= '1';
+		IF CPU_RESET = '1' THEN
+			reset_signal  <= '1';
 			state_machine <= Idle;
 
-		ELSIF RISING_EDGE(clk) THEN
+		ELSIF RISING_EDGE(clk) THEN				
 			CASE state_machine IS
 				WHEN Idle =>
-
-					IF start_signal = '1' OR (testbench = '1' AND GPIO_SW_N = '1') THEN
+				    reset_signal  <= '0';
+				
+					IF GPIO_SW_N = '1' THEN
 						start_flag := '1';
-						reset_signal <= '0';
-
-					ELSIF start_signal = '0' AND start_flag = '1' THEN
+					ELSIF GPIO_SW_N = '0' AND start_flag = '1' THEN
 						start_flag := '0';
 						state_machine <= LoadImage;
-					ELSE
-						reset_signal <= '1';
 					END IF;
 
 				WHEN LoadImage =>
-
-					IF (count < 100 * 2000000) AND testbench = '0' THEN -- temporal to simulate time
-						count := count + 1;
-					ELSE
-						-- TODO this does nothing at the moment
-						loaded_image <= (OTHERS => '0');
-						state_machine <= ProcessImage;
-						count := 0;
-
+					
+					IF img_cont_start = '0' and img_cont_busy = '0' THEN
+					   img_cont_start <= '1';					
+					ELSIF img_cont_busy = '1' THEN
+					   img_cont_start <= '0';						   
+					ELSIF img_cont_done = '1' THEN		   
+					   state_machine <= ProcessImage;				   
 					END IF;
+					
 
 				WHEN ProcessImage =>
-					IF (count < 100 * 2000000) AND testbench = '0' THEN -- temporal to simulate time
-						count := count + 1;
+
+					IF cnn_finished = '1' THEN
+						cnn_start     <= '0';
+						state_machine <= SendResult;
 					ELSE
-
-						IF cnn_finished = '1' THEN
-							cnn_start <= '0';
-							state_machine <= SendResult;
-							count := 0;
-						ELSE
-							cnn_start <= '1';
-						END IF;
-
+						cnn_start <= '1';
 					END IF;
 
 				WHEN SendResult =>
-					IF (count < 100 * 2000000) AND testbench = '0' THEN -- temporal to simulate time
-						count := count + 1;
-					ELSE
+				    -- sends 1 through UART
 
-						IF TX_Done = '1' THEN
-							TX_DV <= '0';
-							state_machine <= Idle;
-							count := 0;
-						ELSE
-							TX_DV <= '1';
-							TX_Byte <= cnn_result & "00";
-						END IF;
-
-					END IF;
+                    IF uart_busy = '1' AND uart_done = '0' THEN
+                        uart_start <= '0';						
+                    ELSIF uart_done = '1' THEN							
+                        state_machine <= Idle;
+                    ELSE
+                        uart_start   <= '1';
+                        uart_msg <= "00110001";							
+                    END IF;
 
 			END CASE;
 		END IF;
