62a9732ec6a1fa353daf0420cf8307cbf51122d0
Johan NIlsson <johnilsa@student.chalmers.se>
2021-05-12 16:37:37 +0200

    Started to implement the FC-layer in VHDL

--- src/vhdl/FullyConnect.vhd
+++ src/vhdl/FullyConnect.vhd
@@ -21,7 +21,7 @@ USE ieee.numeric_std.ALL;
 ENTITY FullyConnect IS
 GENERIC(
   INPUT_NUM        : INTEGER := 9;
-  NEURON_NUM       : INTEGER := 2;
+  NEURON_NUM       : INTEGER := 1;
   IN_SIZE          : INTEGER := 8;
   OUT_SIZE         : INTEGER := 32
 );
@@ -42,11 +42,11 @@ END FullyConnect;
 ARCHITECTURE FullyConnect_arch OF FullyConnect IS
 
 
-  TYPE IN_ARRAY 	IS ARRAY (0 TO INPUT_NUM - 1) OF STD_LOGIC_VECTOR(IN_SIZE - 1 DOWNTO 0);
+--  TYPE IN_ARRAY 	IS ARRAY (0 TO INPUT_NUM - 1) OF STD_LOGIC_VECTOR(IN_SIZE - 1 DOWNTO 0);
   TYPE FILTER_ARRAY IS ARRAY (0 TO NEURON_NUM-1) OF STD_LOGIC_VECTOR(INPUT_NUM*IN_SIZE-1 DOWNTO 0);
   TYPE OUT_ARRAY 	IS ARRAY (0 TO NEURON_NUM - 1) OF STD_LOGIC_VECTOR(OUT_SIZE - 1 DOWNTO 0);
 
-  SIGNAL input_array    : IN_ARRAY;
+--  SIGNAL input_array    : IN_ARRAY;
   SIGNAL weight_array   : FILTER_ARRAY;
   SIGNAL bias_array     : OUT_ARRAY;
   
@@ -92,9 +92,9 @@ ARCHITECTURE FullyConnect_arch OF FullyConnect IS
 
 BEGIN
 
-  init_input_array: FOR i IN 0 TO INPUT_NUM-1 GENERATE
-    input_array(i) <= input( ((i+1)*IN_SIZE-1) DOWNTO (i*IN_SIZE) );
-  END GENERATE; 
+--  init_input_array: FOR i IN 0 TO INPUT_NUM-1 GENERATE
+--    input_array(i) <= input( ((i+1)*IN_SIZE-1) DOWNTO (i*IN_SIZE) );
+--  END GENERATE; 
   
   Neuron_operation: FOR i IN 0 TO NEURON_NUM-1 GENERATE
     weight_array(i)  <= weight_values( ((i+1)*IN_SIZE*INPUT_NUM-1) DOWNTO (i*IN_SIZE*INPUT_NUM) );
@@ -110,17 +110,19 @@ BEGIN
       clk           => clk,
       reset_p       => reset_p,
       enable        => enable,
-      input         => input_array(i),
+      input         => input,
       filter_values => weight_array(i),
       bias          => bias_array(i),
       busy          => neuron_busy_vector(i),
       done          => neuron_done_vector(i),
       output        => neuron_output_array(i)
     );
+    
   END GENERATE;
 
   -- BusyDone : PROCESS(clk)
   -- BEGIN
   -- END;
+    
 
 END FullyConnect_arch;
